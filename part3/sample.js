3fad18cf0f7c0e675059c3e837301a2838b3d934
JHKIMS <wjdgns5131@gmail.com>
2022-11-15 09:32:05 +0900

    ES6_Module식 개발(React,Angular같은 곳 많이 사용)

---
+++ part3/sample.js
@@ -0,0 +1,16 @@
+// let a = 10;
+// let b = 20;
+// export {b};
+// export default a;
+
+
+
+//  여러 개를 할 경우
+ let a = 10;
+ let b = 20;
+ export {a,b}; 
+
+//  export let c = 20;
+//  이렇게도 가능하다.
+
+ 
